363c6efeb8c40d744abccce8cc15d9a0b10f55ce
DogeSSBM <dogessbm@gmail.com>
2021-04-28 17:50:23 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include "Includes.h"
 
+typedef enum{EMPTY, SNAKE, APPLE}Cell;
+
+void drawCell(int x, int y, int scale)
+{
+
+}
+
 int main()
 {
-	const uint scale = 20;
+	const int scale = 20;
 	const Length gridlen = {40, 30};
 	const Length window = coordMul(gridlen, scale);
 	init(window);
+	Cell grid[gridlen.x][gridlen.y];
+
+	printf("%i, %i\n", gridlen.x, gridlen.y);
 
 	while(1){
 		Ticks frameStart = getTicks();
 		clear();
+
 
 
 		draw();
